ff7d2c1125b8299bd95c32ebb490660070f83c30
Connor <connor@cnolandev.com>
2025-04-06 16:13:25 -0700

    add checks to message object

--- message.py
+++ message.py
@@ -45,47 +45,74 @@ class message(object):
         return self
 
     def checkMsgType(self, msgType: msgType):
-        return self.msg[msgAttr.MSG_TYPE.value] == msgType.value
+        if self.hasAttr(msgAttr.MSG_TYPE):
+            return self.msg[msgAttr.MSG_TYPE.value] == msgType.value
+
+        return False
 
     def getMsgType(self):
-        return self.msg[msgAttr.MSG_TYPE.value]
+        if self.hasAttr(msgAttr.MSG_TYPE):
+            return self.msg[msgAttr.MSG_TYPE.value]
+
+        return ""
 
     def setRespType(self, respType: msgRespType):
         self.msg[msgAttr.RESP_TYPE.value] = respType.value
         return self
 
     def checkRespType(self, respType: msgRespType):
-        return self.msg[msgAttr.RESP_TYPE.value] == respType.value
+        if self.hasAttr(msgAttr.RESP_TYPE):
+            return self.msg[msgAttr.RESP_TYPE.value] == respType.value
+
+        return False
 
     def getRespType(self):
-        return self.msg[msgAttr.RESP_TYPE.value]
+        if self.hasAttr(msgAttr.RESP_TYPE):
+            return self.msg[msgAttr.RESP_TYPE.value]
+
+        return ""
 
     def setData(self, data: str | dict):
         self.msg[msgAttr.DATA.value] = data
         return self
 
     def getData(self):
-        return self.msg[msgAttr.DATA.value]
+        if self.hasAttr(msgAttr.DATA):
+            return self.msg[msgAttr.DATA.value]
+
+        return False
 
     def setStatus(self, status: msgStatus):
         self.msg[msgAttr.STATUS.value] = status.value
         return self
 
     def checkStatus(self, status: msgStatus):
-        return self.msg[msgAttr.STATUS.value] == status.value
+        if self.hasAttr(msgAttr.STATUS):
+            return self.msg[msgAttr.STATUS.value] == status.value
+
+        return False
 
     def getStatus(self):
-        return self.msg[msgAttr.STATUS.value]
+        if self.hasAttr(msgAttr.STATUS):
+            return self.msg[msgAttr.STATUS.value]
+
+        return ""
 
     def setCmd(self, cmd: msgCmd):
         self.msg[msgAttr.CMD.value] = cmd.value
         return self
 
     def checkCmd(self, cmd: msgCmd):
-        return self.msg[msgAttr.CMD.value] == cmd.value
+        if self.hasAttr(msgAttr.CMD):
+            return self.msg[msgAttr.CMD.value] == cmd.value
+
+        return False
 
     def getCmd(self):
-        return self.msg[msgAttr.CMD.value]
+        if self.hasAttr(msgAttr.CMD):
+            return self.msg[msgAttr.CMD.value]
+
+        return ""
 
     def buildDict(self):
         if msgAttr.STATUS.value not in self.msg:
